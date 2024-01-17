b08ba334974de174a3719b9e7577dd01bbc0037e
Erdem <zahrakon06@gmail.com>
2024-01-18 02:04:04 +0300

    Update post.rs

--- src/routes/_auth/process/p_id/post.rs
+++ src/routes/_auth/process/p_id/post.rs
@@ -4,7 +4,6 @@ use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use axum::{Extension, Json};
 use axum::extract::Path;
 use axum::http::StatusCode;
-use rand::random;
 use serde::Deserialize;
 use serde_json::{json, Value};
 use sqlx::PgPool;
